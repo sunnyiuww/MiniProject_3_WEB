0b05d7562b4f79d081d612f20d5861ccba33b5c4
sunnyiuww <126841766+sunnyiuww@users.noreply.github.com>
2024-04-18 00:32:59 +0800

    Update about.php

--- view/about.php
+++ view/about.php
@@ -1,12 +1,12 @@
 <?php
 
-include '../php/koneksi.php';
+include 'koneksi.php';
 
 session_start();
 $user_id = $_SESSION['user_id'];
 
 if (!isset($user_id)) {
-  header('location:../php/login.php');
+  header('location:login.php');
 }
 ?>
 
@@ -25,7 +25,7 @@ if (!isset($user_id)) {
 </head>
 
 <body>
-  <?php include('./header-footer/header.php') ?>
+  <?php include('header.php') ?>
   <main class="container-lg ">
 
     <h2 class="text-center fs-1 m-4 fw-medium" style="color: #19376d;">ABOUT FESTIVAL</h2>
@@ -39,8 +39,8 @@ if (!isset($user_id)) {
     <section class="m-5">
     <div class="container p-3">
     <div class="d-flex justify-content-center">
-        <video muted autoplay loop width="2400" height="450">
-            <source src="../assets/img/about.mp4" type="video/mp4">
+        <video muted autoplay loop class="img-fluid" style="max-width: 100%;">
+            <source src="about.mp4" type="video/mp4">
         </video>
     </div>
     </div>
@@ -60,30 +60,30 @@ if (!isset($user_id)) {
         <h1 class="text-center fw-medium">Partnership</h1>
         <div class="row row-cols-2  row-cols-lg-5 g-2 g-lg-3 m-3 d-flex justify-content-center">
           <div class="col">
-            <img src="../assets/img/logo1.jpg" width="180px" height="180px" class="rounded-circle">
+            <img src="logo1.jpg" width="180px" height="180px" class="rounded-circle">
           </div>
           <div class="col">
-            <img src="../assets/img/logo2.jpg" width="180px" height="180px" class="rounded-circle">
+            <img src="logo2.jpg" width="180px" height="180px" class="rounded-circle">
           </div>
           <div class="col">
-            <img src="../assets/img/logo3.png" width="180px" height="180px" class="rounded-circle">
+            <img src="logo3.png" width="180px" height="180px" class="rounded-circle">
           </div>
           <div class="col">
-            <img src="../assets/img/logo4.png" width="180px" height="180px" class="rounded-circle">
+            <img src="logo4.png" width="180px" height="180px" class="rounded-circle">
           </div>
         </div>
         <div class="row row-cols-2  row-cols-lg-5 g-2 g-lg-3 m-3 d-flex justify-content-center">
           <div class="col">
-            <img src="../assets/img/logo5.png" width="180px" height="180px" class="rounded-circle">
+            <img src="logo5.png" width="180px" height="180px" class="rounded-circle">
           </div>
           <div class="col">
-            <img src="../assets/img/logo6.png" width="180px" height="180px" class="rounded-circle">
+            <img src="logo6.png" width="180px" height="180px" class="rounded-circle">
           </div>
           <div class="col">
-            <img src="../assets/img/logo7.jpg" width="180px" height="180px" class="rounded-circle">
+            <img src="logo7.jpg" width="180px" height="180px" class="rounded-circle">
           </div>
           <div class="col">
-            <img src="../assets/img/logo8.jpg" width="180px" height="180px" class="rounded-circle">
+            <img src="logo8.jpg" width="180px" height="180px" class="rounded-circle">
           </div>
         </div>
     </section>
@@ -91,19 +91,19 @@ if (!isset($user_id)) {
       <h1 class="text-center m-4">Team Project</h1>
       <div class="row row-cols-1  row-cols-lg-6 d-flex justify-content-center align-items-center" style="min-height: 30vh;">
         <div class="col card shadow m-3" style="width: 18rem;">
-          <img src="../assets/img/img.jpeg" class="card-img-top p-2" alt="Trio Tahril Rifandi">
+          <img src="img.jpeg" class="card-img-top p-2" alt="sani">
           <div class="card-body text-center">
             <p class="card-text">Rizani Rupa' Mdaun</p>
             <p class="card-text">2209116039</p>
             <p class="card-text">Information System 2022</p>
           </div>
         </div>
-      </div>
+           </div>
     </section>
   </main>
 
 
-  <?php include('./header-footer/footer.php') ?>
+  <?php include('footer.php') ?>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
