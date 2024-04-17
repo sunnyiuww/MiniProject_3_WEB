e05a5b0bd222b223c1fa7c57578a1f4fb17de713
sunnyiuww <126841766+sunnyiuww@users.noreply.github.com>
2024-04-18 00:33:47 +0800

    Update cart.php

--- view/cart.php
+++ view/cart.php
@@ -1,11 +1,11 @@
 <?php
-include '../php/koneksi.php';
+include 'koneksi.php';
 session_start();
 
 $user_id = $_SESSION['user_id'];
 
 if (!isset($user_id)) {
-  header('location:../php/login.php');
+  header('location:login.php');
 }
 
 if (isset($_POST['update_cart'])) {
@@ -57,7 +57,7 @@ if (isset($_GET['delete_all'])) {
     align-items: center;
     justify-content: center;
     gap: 1rem;
-    background: url(../assets//img/cart.jpg) no-repeat;
+    background: url(cart.jpg) no-repeat;
     background-size: cover;
     background-position: center;
     text-align: center;
@@ -131,7 +131,7 @@ if (isset($_GET['delete_all'])) {
 </style>
 
 <body>
-  <?php include('./header-footer/header.php') ?>
+  <?php include('header.php') ?>
   <div class="headings">
     <h1 class="text-white fs-1 fw-bolder text-uppercase">Toraja In Art & Culture Festival Cart</h1>
     <div class="pags rounded" aria-label="breadcrumb">
@@ -193,7 +193,7 @@ if (isset($_GET['delete_all'])) {
 
   </section>
 
-  <?php include('./header-footer/footer.php') ?>
+  <?php include('footer.php') ?>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
